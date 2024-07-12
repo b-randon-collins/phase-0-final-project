2e3adb6e61b90b19cb09a830226d3ee5063d8de7
Brandon Collins <brandonc503@yahoo.com>
2024-07-11 21:05:39 -0400

    Adds pages and css and js

---
+++ MainMenuModule.js
@@ -0,0 +1,10 @@
+window.mainMenuHTML = `
+<nav id="menu-bar">
+    <ul>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About Me</a></li>
+        <li><a href="contact.html">Contact</a></li>
+        <li><a href="projects.html">Projects</a></li>
+    </ul>
+</nav>
+`;
---
+++ script.js
@@ -0,0 +1,5 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+  const headerPlaceholder = document.querySelector("#header-placeholder");
+  headerPlaceholder.innerHTML = window.mainMenuHTML;
+});
